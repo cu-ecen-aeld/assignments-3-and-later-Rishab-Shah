1a31c1f2ac06cd3184c964aabad6117645cb7a5a
Rishab-Shah <rishabshah1994@gmail.com>
2021-09-12 15:30:46 -0600

    removed tree added comments in script,changed path from OLD_PATH to FINDER_APP_DIR

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -25,32 +25,12 @@ bool do_system(const char *cmd)
 */
     int status;
     status = system(cmd);
-
-    //printf("COMMAND_STATUS is %s %d\n",cmd,status);
-
+    
+    printf("=============================================== STATUS = %d, WIFEXITED = %d, WEXITSTATUS = %d\n",status,WIFEXITED(status),WEXITSTATUS(status));
+   
     if(status == 0)
     {
-	syslog(LOG_DEBUG,"%s command executed successfully\n",cmd);
 	return true;
-    }
-    else
-    {
-	//do wait and stuff to check the issues with the child
-#if 0	
-	if(waitpid(pid,&status,0) == -1)
-	{
-	    perror("waitpid");
-	    return false;
-	}
-
-	if(WIFEXITED(status))
-	{
-	    if(WEXITSTATUS(status) !=0)
-	    {
-		return false;
-	    }
-	}
-#endif
     }
 
     return false;
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Script outline to install and build kernel.
 # Author: Siddhant Jajoo.
+# Edited by: Rishab Shah
 
 set -e
 set -u
@@ -13,9 +14,9 @@ FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
 SYSROOT=aarch64-none-linux-gnu-gcc
-OLD_PATH=/home/rishab/assignment-1-Rishab-Shah/finder-app
+# OLD_PATH=/home/rishab/assignment-1-Rishab-Shah/finder-app
 
-
+# Check which directory is used
 if [ $# -lt 1 ]
 then
 	echo "-----Using default directory ${OUTDIR} for output"
@@ -24,12 +25,12 @@ else
 	echo "-----Using passed directory ${OUTDIR} for output"
 fi
 
-#Won't recreate if the directory already exits
 mkdir -p ${OUTDIR}
 
+# Won't recreate if the directory already exits
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
+    	#Clone only if the repository does not exist.
 	echo "-----CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
@@ -39,25 +40,32 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "-----Completed make arm-gcc mrproper step"
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo "-----Completed make arm-gcc defconfig step"
+    make -j4 ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} all
+    echo "-----Completed make arm-gcc all step"
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "-----Completed make arm-gcc modules step"
+    make ARCH=arm64 CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    echo "-----Completed make arm-gcc dtbs step"
 fi 
 
-#TODO: Add Image to outdir
-echo "-----Adding the Image in outdir"
-sudo cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
-echo "-----Creating the staging directory for the root filesystem"
 
+# TODO: Add Image to outdir
+sudo cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
+echo "-----Added the Image in outdir"
+
+
+# Staging
 cd "$OUTDIR"
 if [ -d "${OUTDIR}/rootfs" ]
 then
 	echo "-----Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-
+	sudo rm  -rf ${OUTDIR}/rootfs
 fi
+echo "-----Created the staging directory for the root filesystem"
 
 
 # TODO: Create necessary base directories
@@ -67,12 +75,11 @@ mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
 mkdir -p home/conf
 mkdir -p usr/bin usr/lib usr/sbin
 mkdir -p var/log
-tree -d
-
-
 echo "-----Created necessary base directories"
-cd "$OUTDIR"
 
+
+# Busy box steps
+cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
 git clone git://busybox.net/busybox.git
@@ -84,89 +91,82 @@ git clone git://busybox.net/busybox.git
 else
     cd busybox
 fi
-
 echo "-----Entered busy box"
+
+
 # TODO: Make and install busybox
-echo "====================================HEY HERE++++++++++++++++++++++++++++++++++++"
-
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} CONFIG_PREFIX=$OUTDIR/rootfs install
+echo "-----Installed busybox"
 
-echo "NOW HERE ____________________________________________________________________________________________________________________________________________________"
 
-echo "-----Library dependencies"
+# TODO: Reading binaries
 cd "${OUTDIR}/rootfs"
 ls -alh
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+echo "-----Library dependencies as follows"
 
 
-echo "REACHED HERE"
-export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
-# CD_PATH=$($(SYSROOT) -print-sysroot)
 # TODO: Add library dependencies to rootfs
+export SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
 cd "${SYSROOT}"
 ls -l lib/
-
-echo "-----AGAIN DOING THIS STEP"
-
 ls -l lib/ld-linux-aarch64.so.1
+echo "-----Completed exporting the sysroot path and change of directory"
 
+
+# TODO: Copying files to qemu
 cd "${OUTDIR}/rootfs"
-
-
 sudo cp -L ${SYSROOT}/lib/ld-linux-aarch64.* lib
 sudo cp -L ${SYSROOT}/lib64/libm.so.* lib64
 sudo cp -L ${SYSROOT}/lib64/libresolv.so.* lib64
 sudo cp -L ${SYSROOT}/lib64/libc.so.* lib64
+echo "-----Completed copying libraries to qemu environment"
 
-
-
-
-# sudo cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib
-# sudo cp -a ${SYSROOT}/lib64/libm.so.6 lib64
-# sudo cp -a ${SYSROOT}/lib64/libresolv.so.2 lib64
-# sudo cp -a ${SYSROOT}/lib64/libc.so.6 lib64
-
-
-echo "-----completed copying libraries mentioned above"
 
 # TODO: Make device nodes
 cd "${OUTDIR}/rootfs"
 sudo mknod -m 666 dev/null c 1 3
 sudo mknod -m 600 dev/console c 5 1
-
 ls -l dev
+echo "-----Completed device nodes step"
 
-echo "-----completed device nodes step"
+
 # TODO: Clean and build the writer utility
-cd "${OLD_PATH}"
+cd "${FINDER_APP_DIR}"
+echo "${FINDER_APP_DIR}"
 make clean
 make CROSS_COMPILE=aarch64-none-linux-gnu-
 file writer
-echo "-----wrter utility is built"
+echo "-----Completed wrter utility verification"
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cd "${FINDER_APP_DIR}"
+echo "${FINDER_APP_DIR}"
+sudo cp ${FINDER_APP_DIR}/writer ${OUTDIR}/rootfs/home/
+sudo cp ${FINDER_APP_DIR}/finder.sh  ${OUTDIR}/rootfs/home/
+sudo cp ${FINDER_APP_DIR}/conf/username.txt ${OUTDIR}/rootfs/home/conf/
+sudo cp ${FINDER_APP_DIR}/finder-test.sh ${OUTDIR}/rootfs/home/
+sudo cp ${FINDER_APP_DIR}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
+echo "-----Completed copying files to QEMU environment"
 
-
-cd "${OUTDIR}/rootfs/home/"
-sudo cp ${OLD_PATH}/writer ${OUTDIR}/rootfs/home/
-sudo cp ${OLD_PATH}/finder.sh  ${OUTDIR}/rootfs/home/
-sudo cp ${OLD_PATH}/conf/username.txt ${OUTDIR}/rootfs/home/conf/
-sudo cp ${OLD_PATH}/finder-test.sh ${OUTDIR}/rootfs/home/
-sudo cp ${OLD_PATH}/autorun-qemu.sh ${OUTDIR}/rootfs/home/
 
 # TODO: Chown the root directory
 cd "${OUTDIR}"
 sudo chown -R root:root *
+echo "-----Completed chown step"
+
 
 # TODO: Create initramfs.cpio.gz
 cd "${OUTDIR}/rootfs"
 find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
-echo "-----completed initramfs step find"
+echo "-----Completed initramfs step"
 
+
+# TODO: Gzip step as follows
 cd ..
 gzip -f initramfs.cpio
-
-echo "-----gzip completed"
+echo "-----Completed gzip"
 
