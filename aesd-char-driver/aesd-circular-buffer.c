4c3c6805d0c8a9c69907c7b435156f4992fcb207
Rishab-Shah <rishabshah1994@gmail.com>
2021-10-16 13:20:59 -0600

    added aesd char driver circular buffer. Passed local full test

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -8,6 +8,9 @@
  *
  */
 
+/*
+https://www.youtube.com/watch?v=uvD9_Wdtjtw
+*/
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
@@ -32,7 +35,52 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+    //NULL check
+    if( (buffer == NULL) || (entry_offset_byte_rtn == NULL))
+    {
+        printf("execute - 2?\n");
+        return NULL;
+    }
+    
+    //capture current status
+    uint8_t curr_ref_position = buffer->out_offs;
+    size_t curr_ref_position_size = buffer->entry[curr_ref_position].size;
+    int prev_char_position = 0;
+    
+    //less than case
+    while(1)
+    {
+      if(char_offset < curr_ref_position_size)
+      {
+        *entry_offset_byte_rtn = (char_offset - prev_char_position);
+        return &buffer->entry[curr_ref_position];
+      }
+      //next location required to reference, as the index of 
+      //the character requested is more than the size of the content is
+      else
+      {
+        //take size of previous entry , number of characters pseudo traversed till now
+        prev_char_position = curr_ref_position_size;
+        
+        //increment to the next location to make the proper return when hit
+        curr_ref_position = ( (curr_ref_position + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED);
+        
+        //find the size for existing entry and add it to previous, to kniow if next
+        //condition woudld pass or not
+        curr_ref_position_size += buffer->entry[curr_ref_position].size;
+         
+      }
+      
+      //break when the traversing has reached to the same location
+      if(curr_ref_position == buffer->out_offs)
+      {
+        break;
+      }
+      
+    }
+    
     return NULL;
+
 }
 
 /**
@@ -47,6 +95,30 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description 
     */
+    
+    //null check
+    if((buffer == NULL) || (add_entry == NULL))
+    {
+      printf("execute - 1?\n");
+      return;
+    }
+    
+    //add content to the buffer as the data has passed error check 
+    buffer->entry[buffer->in_offs++] = *add_entry;
+    buffer->in_offs = (buffer->in_offs % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED);
+    
+    //for the given entry addition , check the relative read(out) position
+    if(buffer->full == true)
+    {
+      buffer->out_offs++;
+      buffer->out_offs = (buffer->out_offs % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED);
+    }
+
+    //check for buffer to be full now? by checking the read(out) and write(in) pointer locations
+    if(buffer->in_offs == buffer->out_offs)
+    {
+      buffer->full = true;
+    }
 }
 
 /**
@@ -56,3 +128,5 @@ void aesd_circular_buffer_init(struct aesd_circular_buffer *buffer)
 {
     memset(buffer,0,sizeof(struct aesd_circular_buffer));
 }
+
+/* EOF */
